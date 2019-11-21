7baca8d5059182dc4ce7fb0740b62566bbd65f62
Julian Gruber <julian@juliangruber.com>
2019-11-21 16:37:54 +0100

    add store content in memory

--- server.js
+++ server.js
@@ -3,8 +3,21 @@
 
 const http = require('http')
 
+let content = 'DATA'
+
 const server = http.createServer((req, res) => {
-  res.end('DATA')
+  console.log(req.method, req.url)
+
+  if (req.method === 'GET') {
+    res.end(content)
+  } else if (req.method === 'PUT') {
+    let data = ''
+    req.on('data', d => data += d.toString())
+    req.on('end', () => {
+      content = data
+      res.end()
+    })
+  }
 })
 
 server.listen(8080)
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import { Wax, CreateSchema } from 'wax-prosemirror-core'
 import { DefaultSchema } from 'wax-prosemirror-schema'
 import useSWR from 'swr'
@@ -10,10 +10,31 @@ const options = {
 const fetcher = url => fetch(url).then(r => r.text())
 
 const App = () => {
+  const [content, setContent] = useState(null)
+  const [modified, setModified] = useState(false)
+
+  const onChange = content => {
+    setModified(true)
+    setContent(content)
+  }
+
   const { data } = useSWR('/content', fetcher)
   if (!data) return null
+
+  const save = async () => {
+    if (!content) return
+    setModified(false)
+    await fetch('/content', {
+      method: 'PUT',
+      body: content
+    })
+  }
+
   return (
-    <Wax options={options} autoFocus value={data} debug />
+    <Fragment>
+      <button onClick={save} disabled={!content || !modified}>Save</button>
+      <Wax options={options} autoFocus value={data} debug onChange={onChange} />
+    </Fragment>
   )
 }
 
