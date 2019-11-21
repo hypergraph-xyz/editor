f0c5eeb29c4897fae2cfbf0c270dd70893173c81
Julian Gruber <julian@juliangruber.com>
2019-11-21 17:09:50 +0100

    add static file server for build/

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 'use strict'
 
 const http = require('http')
+const serveStatic = require('serve-static')
 
 let content = 'DATA'
+
+const serve = serveStatic(`${__dirname}/build`)
 
 const server = http.createServer((req, res) => {
   console.log(req.method, req.url)
 
   if (req.method === 'GET') {
-    res.end(content)
+    serve(req, res, () => {
+      res.end(content)
+    })
   } else if (req.method === 'PUT') {
     let data = ''
     req.on('data', d => (data += d.toString()))
